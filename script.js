22946ab2e8a3e58cf35e950b4736929f7d1f5ede
caroline cloughley <caro672g@stud.kea.dk>
2022-08-30 09:23:40 +0200

    first value console logs

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 "use strict";
+
+window.addEventListener("load", startComputer);
+
+let result;
+
+function startComputer() {
+  document.querySelector("#outButton").addEventListener("click", output);
+}
+
+function output() {
+  let input = document.getElementById("inputText").value;
+  let outputs = document.getElementById("outputs").value;
+  if (outputs == 1) {
+    let result = input[0].toUpperCase() + input.substring(1).toLowerCase();
+    console.log(result);
+  }
+}
