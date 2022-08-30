1fd5afdcb624bf38406ccd3d2afcc23b62371a8c
caroline cloughley <caro672g@stud.kea.dk>
2022-08-30 09:35:26 +0200

    first two options work

--- script.js
+++ script.js
@@ -10,9 +10,15 @@ function startComputer() {
 
 function output() {
   let input = document.getElementById("inputText").value;
-  let outputs = document.getElementById("outputs").value;
-  if (outputs == 1) {
+  let options = document.getElementById("options").value;
+  if (options == 1) {
     let result = input[0].toUpperCase() + input.substring(1).toLowerCase();
     console.log(result);
+    document.getElementById("outputText").value = result;
+  }
+  if (options == 2) {
+    let result = input.split(" ")[0];
+    console.log(result);
+    document.getElementById("outputText").value = result;
   }
 }
